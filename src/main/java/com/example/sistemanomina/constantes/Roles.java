48445a1aa2e5bd2e486d1810c3e793dec2b1655d
DanielReyes03 <daniel_reyes2003@outlook.com>
2025-07-31 20:03:59 -0600

    adding user layout

---
+++ src/main/java/com/example/sistemanomina/constantes/Roles.java
@@ -0,0 +1,15 @@
+// Realizado por Fredy Reyes 0901-22-9800
+package com.example.sistemanomina.constantes;
+
+public class Roles {
+
+    private Roles() {
+
+    }
+
+    public static final String ADMIN = "ADMIN";
+    public static final String EMPLEADO = "EMPLEADO";
+    public static final String GERENTE = "GERENTE";
+    public static final String CONTADOR = "CONTADOR";
+
+}
